f22160228466c9ff9fe59354d92ab787c04a07f0
Azib M. Javaid <azibmazhar@gmail.com>
2025-09-03 21:07:09 +0500

    Initial Commit

---
+++ Test Project/Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ProjectApp.swift
+//  Test Project
+//
+//  Created by Azib M. Javaid on 03/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
